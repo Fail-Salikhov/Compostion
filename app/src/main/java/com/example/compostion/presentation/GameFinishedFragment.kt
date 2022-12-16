b86798200f0eb777c67c2382c2eeef32f63df80d
Fail Salikhov <sff1313@yandex.ru>
2022-12-16 15:52:14 +0400

    подключил dataBind

--- app/src/main/java/com/example/compostion/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/compostion/presentation/GameFinishedFragment.kt
@@ -45,42 +45,11 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun bindViews() {
-        with(binding) {
-            emojiResult.setImageResource(getSmileResId())
-            tvRequiredAnswers.text = String.format(
-                getString(R.string.required_score),
-                args.gameResult.gameSettings.minCountRightOfAnswers
-            )
-            tvScoreAnswers.text = String.format(
-                getString(R.string.score_answers),
-                args.gameResult.countOfRightAnswers
-            )
-            tvRequiredPercentage.text = String.format(
-                getString(R.string.required_percentage),
-                args.gameResult.gameSettings.minPercentOfRightAnswers
-            )
-            tvScorePercentage.text = String.format(
-                getString(R.string.score_percentage),
-                getPercentOfRightAnswers()
-            )
-        }
+        binding.gameResult = args.gameResult
+
     }
 
-    private fun getSmileResId(): Int {
-        return if (args.gameResult.winner) {
-            R.drawable.ic_smile
-        } else {
-            R.drawable.ic_sad
-        }
-    }
 
-    private fun getPercentOfRightAnswers() = with(args.gameResult) {
-        if (countOfQuestions == 0) {
-            0
-        } else {
-            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
-        }
-    }
 
     override fun onDestroyView() {
         super.onDestroyView()
